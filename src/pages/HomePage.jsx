f78d3f7acae8598004448ecfe1685489a7bdd595
anupkumarjana <janaanup7872@gmail.com>
2024-04-16 21:35:52 +0530

    home  page completed

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Hero from "../components/Home/Hero";
+
+const HomePage = () => {
+  return (
+    <div className="lg:px-20 px-10 pt-40 lg:pt-0">
+      <Hero />
+    </div>
+  );
+};
+
+export default HomePage;
