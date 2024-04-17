c9b6728a18277d0235a568ec8f4ad5e583d05546
anupkumarjana <janaanup7872@gmail.com>
2024-04-17 13:12:39 +0530

    fixed routes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./custom.css";
-import App from "./App";
+// import App from "./App";
+import { RouterProvider } from "react-router-dom";
+import { AppLayout } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<RouterProvider router={AppLayout} />);
