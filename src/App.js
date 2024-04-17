6700c011ccf8ab0d66c85c82399d8726c4b13649
anupkumarjana <janaanup7872@gmail.com>
2024-04-17 13:12:10 +0530

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
+import BlogsPage from "./pages/BlogsPage";
+import ContactPage from "./pages/ContactPage";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
       <Header />
-      <HomePage />
+      <Outlet />
     </div>
   );
 };
+export const AppLayout = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/services",
+        element: <ServicesPage />,
+      },
+      {
+        path: "/blogs",
+        element: <BlogsPage />,
+      },
+      {
+        path: "/contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
 
 export default App;
