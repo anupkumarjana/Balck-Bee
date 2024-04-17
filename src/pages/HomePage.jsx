cd5d066f2fce8caa4746291a85bd77eab1ffe421
anupkumarjana <janaanup7872@gmail.com>
2024-04-17 13:11:25 +0530

    HomePage with Hero, BuisnessGrowth, Banner completed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Hero from "../components/Home/Hero";
+import BuisnessGrowth from "../components/Home/BuisnessGrowth";
+import Banner from "../components/Home/Banner";
 
 const HomePage = () => {
   return (
-    <div className="lg:px-20 px-10 pt-40 lg:pt-0">
+    <div className="lg:px-32 px-10  pt-20 lg:pt-40 flex-col gap-20  lg:text-start text-center text-wrap ">
       <Hero />
+      <div className="flex flex-col gap-40">
+        <div>
+          <BuisnessGrowth />
+        </div>
+        <div>
+          <Banner />
+        </div>
+      </div>
     </div>
   );
 };
