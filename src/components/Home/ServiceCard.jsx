0b94454b201bd141e75493a68393cdb912fd58c6
anupkumarjana <janaanup7872@gmail.com>
2024-04-18 23:15:55 +0530

    service card for offer component complted

---
+++ src/components/Home/ServiceCard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const ServiceCard = ({data}) => {
+  return (
+    <div className="w-full h-96 flex flex-col gap-6 bg-[#343434] text-white p-10 rounded-xl">
+      <div className="flex flex-wrap justify-start items-center gap-4">
+        <span className="lg:text-6xl text-3xl text-orange-500">{data.icon}</span>
+        <span className="lg:text-2xl text-xl font-montserrat">{data.title}</span>
+      </div>
+      <div>{data.content}</div>
+    </div>
+  );
+}
+
+export default ServiceCard
