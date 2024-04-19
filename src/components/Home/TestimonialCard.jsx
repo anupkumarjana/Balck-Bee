8a9d128c35f31369fb92511a89b162d1794ac034
anupkumarjana <janaanup7872@gmail.com>
2024-04-19 15:29:25 +0530

    bg-color chnaged

--- src/components/Home/TestimonialCard.jsx
+++ src/components/Home/TestimonialCard.jsx
@@ -3,14 +3,20 @@ import { FaQuoteLeft } from "react-icons/fa6";
 
 const TestimonialCard = ({ testimonial }) => {
   return (
-    <div className="max-w-96 w-auto h-auto border rounded-xl shadow-xl mx-2">
+    <div className="max-w-96 w-auto h-auto border border-slate-400 outline-none rounded-xl shadow-xl mx-2">
       <div className="flex flex-col ">
-        <div className="text-wrap p-6 bg-blue-500 font-montserrat text-sm text-white">
-        <span className="text-2xl text-orange-500"><FaQuoteLeft/></span>
-          <p>{testimonial.content.slice(0,60)}</p>
+        <div className="text-wrap p-6 bg-[#002244] font-montserrat text-sm text-white rounded-xl">
+          <span className="text-2xl text-orange-500">
+            <FaQuoteLeft />
+          </span>
+          <p>{testimonial.content}</p>
         </div>
         <div className="flex gap-4 p-6 justify-start items-center">
-          <img src={testimonial.image} alt="" className="w-10 rounded-full outline outline-white"/>
+          <img
+            src={testimonial.image}
+            alt=""
+            className="w-10 rounded-full outline outline-white"
+          />
           <div className="flex flex-col font-outfit text-start">
             <span>{testimonial.name}</span>
             <span className="text-orange-500">{testimonial.organisation}</span>
