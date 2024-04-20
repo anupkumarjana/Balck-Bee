ced9af71dbe65b3326843ec594765247eac352e9
anupkumarjana <janaanup7872@gmail.com>
2024-04-20 17:04:17 +0530

    service route media buying page created and completed

---
+++ src/pages/MediaBuyingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MediaBuying = () => {
+  return (
+     <main className="pt-40 flex-col gap-20  lg:text-start text-center text-wrap">MediaBuying</main>
+  )
+}
+
+export default MediaBuying
