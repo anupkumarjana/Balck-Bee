78313d94235cfc8c3bb8b1a573a9753ddd09998f
anupkumarjana <janaanup7872@gmail.com>
2024-04-20 17:03:27 +0530

    service route content marketing page

---
+++ src/pages/ContentMarketingPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ContentMarketingPage = () => {
+  return (
+      <main className="pt-40 flex-col gap-20  lg:text-start text-center text-wrap">ContentMarketingPage</main>
+  )
+}
+
+export default ContentMarketingPage
