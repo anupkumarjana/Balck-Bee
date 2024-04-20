1525f21cf2529c14a5572ab0acebcdf45c36188a
anupkumarjana <janaanup7872@gmail.com>
2024-04-20 17:06:57 +0530

    created service page routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
-import ServicesPage from "./pages/ServicesPage";
 import BlogsPage from "./pages/BlogsPage";
 import ContactPage from "./pages/ContactPage";
+import PackagesPage from "./pages/PackagesPage";
+import ContentMarketingPage from "./pages/ContentMarketingPage";
+import MediaBuyingPage from "./pages/MediaBuyingPage";
 import { createBrowserRouter, Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import { ThemeProvider } from "styled-components";
-import { GlobalStyles } from "./darkmode/globalStyles";
+import {
+  GlobalStyles,
+  setThemeMode,
+  getThemeMode,
+} from "./darkmode/globalStyles";
 import { lightTheme, darkTheme } from "./darkmode/Theme";
 
 const App = () => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState(getThemeMode());
+
+  useEffect(() => {
+    setThemeMode(theme);
+  }, [theme]);
+
   const themeToggler = () => {
     theme === "light" ? setTheme("dark") : setTheme("light");
   };
+
   return (
     <div className=" ">
       <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
         <>
           <GlobalStyles />
-          <Header themeToggler={themeToggler} />
+          <Header themeToggler={themeToggler} themeMode={theme} />
           <Outlet />
           <Footer />
         </>
@@ -29,6 +41,7 @@ const App = () => {
     </div>
   );
 };
+
 export const AppLayout = createBrowserRouter([
   {
     path: "/",
@@ -44,7 +57,21 @@ export const AppLayout = createBrowserRouter([
       },
       {
         path: "/services",
-        element: <ServicesPage />,
+        // element: <ServicesPage />,
+        children: [
+          {
+            path: "/services/packages",
+            element: <PackagesPage />,
+          },
+          {
+            path: "/services/content-marketing",
+            element: <ContentMarketingPage />,
+          },
+          {
+            path: "/services/media-buying",
+            element: <MediaBuyingPage />,
+          },
+        ],
       },
       {
         path: "/blogs",
