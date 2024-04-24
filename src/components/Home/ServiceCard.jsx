98aac934dd0a97b29807692a26e4641d59c52a50
anupkumarjana <janaanup7872@gmail.com>
2024-04-24 12:16:07 +0530

    service card size chnaged

--- src/components/Home/ServiceCard.jsx
+++ src/components/Home/ServiceCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ServiceCard = ({ data }) => {
   return (
-    <div className="w-full h-96 flex flex-col gap-6 bg-[#002244] text-white p-10 rounded-xl">
+    <div className="w-full h-96 flex flex-col gap-6 bg-[#002244] text-white p-10 rounded-xl outline outline-2 border-black border-2">
       <div className="flex flex-wrap justify-start items-center gap-4">
         <span className="lg:text-6xl text-3xl text-orange-500">
           {data.icon}
