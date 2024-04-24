74c8202b030a9655e1a895a9f0b3ccc5c49e1934
anupkumarjana <janaanup7872@gmail.com>
2024-04-24 11:14:48 +0530

    team cards updated

--- src/components/About/OurTeam.jsx
+++ src/components/About/OurTeam.jsx
@@ -5,13 +5,16 @@ import { ourTeam } from "../../utils/OurTeam";
 const OurTeam = () => {
   return (
     <div className="flex flex-col gap-20">
-      <h3
-        className="text-lg text-orange-500 font-montserrat text-center"
-        data-aos="fade-up"
-      >
-        OUR TEAM
-      </h3>
-      <div className="w-full flex flex-wrap justify-center items-center gap-6">
+      <div className="font-montserrat flex flex-col gap-2 justify-center items-center text-center mb-4">
+        <h3 className="text-lg text-orange-500 " data-aos="fade-up">
+          OUR TEAM
+        </h3>
+        <p className="lg:w-96 text-wrap" data-aos="fade-up">
+          Connect with us to explore endless possibilities and elevate your
+          brand's digital presence.
+        </p>
+      </div>
+      <div className="w-full flex flex-wrap justify-center items-center gap-10">
         {ourTeam.map((member, index) => (
           <a
             key={index}
@@ -21,7 +24,7 @@ const OurTeam = () => {
           >
             <div
               key={index}
-              className="w-64 h-full border rounded-xl shadow-2xl hover:scale-105 transition-all ease-in-out duration-300 flex flex-col gap-4 justify-center items-center text-center bg-[#002244]"
+              className="w-52 h-full border rounded-xl shadow-2xl hover:scale-105 transition-all ease-in-out duration-300 flex flex-col gap-4 justify-center items-center text-center bg-[#002244]"
             >
               <img
                 src={member.img}
@@ -30,10 +33,10 @@ const OurTeam = () => {
                 // Adjust 'h-40' to set the desired height for the images
               />
               <div className=" text-wrap flex flex-col justify-center gap-1 font-montserrat p-4">
-                <span className="text-lg font-medium text-white">
+                <span className="text-sm font-medium text-white">
                   {member.name}
                 </span>
-                <span className="text-sm font-light text-orange-500">
+                <span className="text-xs font-normal text-orange-500">
                   {member.role}
                 </span>
               </div>
