aa6a0c85addc03df7486d1e6a557ac181eb19d19
anupkumarjana <janaanup7872@gmail.com>
2024-04-25 10:30:04 +0530

    added the hero component

--- src/pages/ContentMarketingPage.jsx
+++ src/pages/ContentMarketingPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ContentMarketing from '../components/Services/ContentMarketing'
 
 const ContentMarketingPage = () => {
   return (
-      <main className="pt-40 flex-col gap-20  lg:text-start text-center text-wrap">ContentMarketingPage</main>
-  )
+    <main className="lg:px-40 px-10  pt-40 flex-col gap-20  lg:text-start text-center text-wrap">
+      <ContentMarketing />
+    </main>
+  );
 }
 
 export default ContentMarketingPage
