fcc791f78f9598d182083966ef7f9004037c82f1
anupkumarjana <janaanup7872@gmail.com>
2024-04-25 10:31:07 +0530

    content marketing card data

---
+++ src/utils/ContentMarketingData.js
@@ -0,0 +1,27 @@
+import { RiArticleFill } from "react-icons/ri";
+import { MdEmail } from "react-icons/md";
+import { PiPackageFill } from "react-icons/pi";
+import { MdWeb } from "react-icons/md";
+
+export const ContentMarketingData = [
+  {
+    icon: <RiArticleFill />,
+    title: "Blog Articles",
+    description: "Lorem ipsum dolor",
+  },
+  {
+    icon: <MdEmail />,
+    title: "Email Writing",
+    description: "Lorem ipsum dolor",
+  },
+  {
+    icon: <PiPackageFill />,
+    title: "Product Captions",
+    description: "Lorem ipsum dolor",
+  },
+  {
+    icon: <MdWeb />,
+    title: "Website Content",
+    description: "Lorem ipsum dolor",
+  },
+];
