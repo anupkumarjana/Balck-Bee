4f9bdd4e7c72075129942ee4fceac7f93404dc32
anupkumarjana <janaanup7872@gmail.com>
2024-05-03 19:44:14 +0530

    content marketing card styling chnaged and sticky scroll

--- src/components/Services/ContentMarketingCard.jsx
+++ src/components/Services/ContentMarketingCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const ContentMarketingCard = ({ data }) => {
   return (
-    <div className="w-60 flex flex-col gap-20 border rounded-xl p-4 shadow-2xl">
+    <div className="lg:w-60 w-full flex flex-col gap-20 border rounded-xl p-4 shadow-2xl">
       <span className="text-6xl text-orange-500 font-semibold">{data.icon}</span>
       <div className="flex flex-col gap-2">
          <span className="text-xl font-montserrat font-bold">{data.title}</span>
-      <span className="text-lg font-outfit font-light text-zinc-700">{data.description}</span>
+      <span className="text-lg font-outfit font-light">{data.description}</span>
       </div>
      
 
