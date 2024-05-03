6a66bd9e09934bf9ef9e531b40a2ab00d56534e3
anupkumarjana <janaanup7872@gmail.com>
2024-05-03 19:43:25 +0530

    hero section button chnaged

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import hero from "../../assets/Home/hero2.png";
 import underline from "../../assets/Home/shape-03.svg";
 import arrowImage from "../../assets/Home/sip-b-arrow.decb954d.svg";
+import { Link } from "react-router-dom";
+
 
 const Hero = () => {
   return (
@@ -31,16 +33,15 @@ const Hero = () => {
             />
           </div>
 
-          <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto
-            laboriosam sit ut ullam ipsam culpa odio{" "}
+          <p className="">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
           </p>
           <div className="flex gap-4 flex-wrap" data-aos="fade-up">
             {/* <button className="border rounded-md px-4 py-2 bg-purple-600 text-white button">
               Find Solution
             </button> */}
             <button className="text-lg font-montserrat font-medium flex items-center gap-2 rounded-xl lg:py-4 lg:px-8 px-4 py-2 bg-[#FF4D30] text-white shadow-custom hover:shadow-custom-hovered transition-all duration-300 ease-in-out  button">
-              Find Solution
+            <Link to="/contact">Find Solution</Link>
             </button>
             <button className="text-lg font-montserrat font-medium  border rounded-xl px-4 py-2 button hover:bg-black hover:text-white transition-all ease-in-out duration-200">
               Learn more
