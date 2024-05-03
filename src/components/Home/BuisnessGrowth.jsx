00f600e3c8e48416ba16a347bb2646ccbd000eb9
anupkumarjana <janaanup7872@gmail.com>
2024-05-03 19:42:55 +0530

    sticky scroll added

--- src/components/Home/BuisnessGrowth.jsx
+++ src/components/Home/BuisnessGrowth.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import about from "../../assets/Home/about-01.jpg";
 import { HiMiniCheckBadge } from "react-icons/hi2";
@@ -33,7 +34,7 @@ const BuisnessGrowth = () => {
             className="text-lg font-montserrat font-medium flex items-center gap-2 rounded-lg py-4 px-8 bg-[#FF4D30] text-white shadow-custom hover:shadow-custom-hovered transition-all duration-300 ease-in-out "
             data-aos="fade-up"
           >
-            Contact us
+            <Link to="/contact">Contact us</Link>
           </button>
         </div>
 
@@ -44,36 +45,54 @@ const BuisnessGrowth = () => {
           >
             BUSINESS GROWTH
           </span>
-          <h3 className="lg:text-6xl text-2xl font-montserrat lg:py-6"   data-aos="fade-up"> 
+          <h3
+            className="lg:text-6xl text-2xl font-montserrat lg:py-6"
+            data-aos="fade-up"
+          >
             Best Digital <br /> Creative Agency
           </h3>
-          <p className="text-lg font-normal font-montserrat "  data-aos="fade-up">
+          <p
+            className="text-lg font-normal font-montserrat "
+            data-aos="fade-up"
+          >
             There are many variations of passages of lorem in free market to
             available, but the majority have suffered alteration in some form,
             by injected humour, or randomised words
           </p>
 
           <div className="flex flex-col gap-4 w-full font-montserrat">
-            <div className="flex lg:gap-4 gap-2 items-center justify-start border rounded-md lg:text-xl text-sm lg:w-full p-4 shadow-lg flex-shrink text-start"  data-aos="fade-up">
+            <div
+              className="flex lg:gap-4 gap-2 items-center justify-start border rounded-md lg:text-xl text-sm lg:w-full p-4 shadow-lg flex-shrink text-start"
+              data-aos="fade-up"
+            >
               <span className="text-orange-500">
                 <HiMiniCheckBadge />
               </span>{" "}
               <p>For marketing professionals</p>
             </div>
-            <div className="flex lg:gap-4 gap-2 items-center justify-start border rounded-md lg:text-xl text-sm lg:w-full p-4 shadow-lg flex-shrink text-start"  data-aos="fade-up">
+            <div
+              className="flex lg:gap-4 gap-2 items-center justify-start border rounded-md lg:text-xl text-sm lg:w-full p-4 shadow-lg flex-shrink text-start"
+              data-aos="fade-up"
+            >
               <span className="text-orange-500">
                 <HiMiniCheckBadge />
               </span>{" "}
               <p>The New Event Marketing Opportunity</p>
             </div>
-            <div className="flex lg:gap-4 gap-2 items-center justify-start border rounded-md lg:text-xl text-sm lg:w-full p-4 shadow-lg flex-shrink text-start"  data-aos="fade-up">
+            <div
+              className="flex lg:gap-4 gap-2 items-center justify-start border rounded-md lg:text-xl text-sm lg:w-full p-4 shadow-lg flex-shrink text-start"
+              data-aos="fade-up"
+            >
               <span className="text-orange-500">
                 <HiMiniCheckBadge />
               </span>{" "}
               <p>The New Event Marketing Opportunity</p>
             </div>
           </div>
-          <p className="text-lg font-normal font-montserrat "  data-aos="fade-up">
+          <p
+            className="text-lg font-normal font-montserrat "
+            data-aos="fade-up"
+          >
             Variations of passages of lorem in free market to available, but the
             majority have suffered alteration in some form, by injected humour,
             or randomised words
