9782d7ce1ccce6107f9ac606ca966f3b4d410db9
anupkumarjana <janaanup7872@gmail.com>
2024-05-03 21:02:26 +0530

    created media buying page

--- src/pages/MediaBuyingPage.jsx
+++ src/pages/MediaBuyingPage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import MediaBuyingHero from "../components/Services/MediaBuyingHero";
+import PackageSection from "../components/Services/PackageSection";
+import Banner from "../components/Home/Banner";
 
 const MediaBuying = () => {
   return (
-     <main className="pt-40 flex-col gap-20  lg:text-start text-center text-wrap">MediaBuying</main>
-  )
-}
+    <main className="lg:px-40 px-10 pt-40 flex flex-col gap-20 lg:gap-40  lg:text-start text-center text-wrap ">
+      <div>
+        <MediaBuyingHero />
+      </div>
+      <div>
+        <Banner />
+      </div>
+      <div>
+        <PackageSection />
+      </div>
+    </main>
+  );
+};
 
-export default MediaBuying
+export default MediaBuying;
