64087de01af087c861d5ee6b6b43557f8330a52a
anupkumarjana <janaanup7872@gmail.com>
2024-05-03 19:47:05 +0530

    modified

--- src/pages/ContentMarketingPage.jsx
+++ src/pages/ContentMarketingPage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import ContentMarketing from '../components/Services/ContentMarketing'
+import ContentMarketingHero from '../components/Services/ContentMarketingHero'
+import PackageSection from "../components/Services/PackageSection";
 
 const ContentMarketingPage = () => {
   return (
-    <main className="lg:px-40 px-10  pt-40 flex-col gap-20  lg:text-start text-center text-wrap">
-      <ContentMarketing />
+    <main className="lg:px-40 px-10 pt-40 flex flex-col gap-20 lg:gap-40  lg:text-start text-center text-wrap ">
+      <div>
+        <ContentMarketingHero />
+      </div>
+      <div>
+        <PackageSection />
+      </div>
     </main>
   );
 }
